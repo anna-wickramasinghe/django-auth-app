a1276a40df40612c46bfd7a0e20febe39d8fb3d3
anna-wickramasinghe <nprawickramasinghe@gmail.com>
2024-04-17 13:42:25 +0530

    initial commit, mostly completed the backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import react from "react"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
